81df594402467b85c093548116a9df19d9309a47
DimitarMilushev <DeadlyCookye@gmail.com>
2019-02-08 00:40:38 +0200

    .

---
+++ JS-Fundamentals/DOM/04_Locked-Profile/solution.js
@@ -0,0 +1,32 @@
+function solve() {
+
+     Array.from(document.getElementsByTagName("button")).forEach((btn) => {
+      btn.addEventListener("click", showProfile);
+  });
+
+    let currentProfile;
+    let hideElementSuffix = "HiddenFields";
+    
+    function showProfile(event)
+    {      
+       currentProfile = event.target.parentNode;
+       let childrenElements = currentProfile.children;
+       let button = childrenElements[10];
+       let radioButton = childrenElements[4];
+
+       let currentProfileName = radioButton.name;
+       currentProfileName = currentProfileName.substring(0, currentProfileName.length - 6);
+       let hiddenFieldsElement = childrenElements[9];
+       
+       if(hiddenFieldsElement.id && radioButton.checked){
+
+       hiddenFieldsElement.id = "";
+       button.textContent = "Hide it";
+       
+       } else {
+         hiddenFieldsElement.id = currentProfileName + hideElementSuffix;
+         button.textContent = "Show more";
+
+       }
+    }
+} 
