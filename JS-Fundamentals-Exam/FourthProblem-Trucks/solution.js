d7e5af3e78a952e141ecb9fa2d38c950e0ab07ca
DimitarMilushev <DeadlyCookye@gmail.com>
2019-03-15 17:38:51 +0200

    started FourthProblem-Trucks/solution.js

--- JS-Fundamentals-Exam/FourthProblem-Trucks/solution.js
+++ JS-Fundamentals-Exam/FourthProblem-Trucks/solution.js
@@ -1,3 +1,28 @@
 function solve() {
-    // TODO
+
+    let buttons = document.getElementsByTagName("button");
+   
+    button[0].addEventListener("click", addNewTruck);
+    button[1].addEventListener("click", addNewTires);
+    button[2].addEventListener("click", goToWork);
+    button[3].addEventListener("click", endShift);
+    
+    function addNewTruck() {
+
+    }
+
+    function addNewTires() {
+
+
+    }
+
+    function goToWork() {
+
+
+    }
+
+    function endShift() {
+
+        
+    }
 }
