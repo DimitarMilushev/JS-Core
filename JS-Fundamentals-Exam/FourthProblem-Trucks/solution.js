749c3b80ca9cc0e247adecc4ab1ce65d147a25cf
DimitarMilushev <DeadlyCookye@gmail.com>
2019-03-15 22:53:22 +0200

    .

--- JS-Fundamentals-Exam/FourthProblem-Trucks/solution.js
+++ JS-Fundamentals-Exam/FourthProblem-Trucks/solution.js
@@ -2,26 +2,40 @@ function solve() {
 
     let buttons = document.getElementsByTagName("button");
    
-    button[0].addEventListener("click", addNewTruck);
-    button[1].addEventListener("click", addNewTires);
-    button[2].addEventListener("click", goToWork);
-    button[3].addEventListener("click", endShift);
-    
-    function addNewTruck() {
+    buttons[0].addEventListener("click", addNewTruck);
+    buttons[1].addEventListener("click", addNewTires);
+    buttons[2].addEventListener("click", goToWork);
+    buttons[3].addEventListener("click", endShift);
+
+    let trucks = {};
+    let backupTires = {};
+
+    function addNewTruck(event) {
+      
+         let plateNumber = document.getElementById("newTruckPlateNumber").value;
+         let tiresCondition = document.getElementById("newTruckTiresCondition").value.split(" ");
+
+         trucks.push({
+             plateNumber: plateNumber,
+             tires: tiresCondition,
+         });
+    }
+
+    function addNewTires(event) {
+
+        let tiresCondition = document.getElementById("newTiresCondition").value.split(" ");
+        
+        tiresCondition.forEach(function(tire) {
+            backupTires.push(tire);
+        });
+    }
+
+    function goToWork(event) {
+
 
     }
 
-    function addNewTires() {
-
-
-    }
-
-    function goToWork() {
-
-
-    }
-
-    function endShift() {
+    function endShift(event) {
 
         
     }
