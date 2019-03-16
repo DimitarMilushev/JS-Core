77e5cf64d52356f18b9d596c0053fc0b46b48bf4
DimitarMilushev <DeadlyCookye@gmail.com>
2019-03-16 11:02:51 +0200

    .

--- JS-Fundamentals-Exam/SecondProblem-Crossword/solution.js
+++ JS-Fundamentals-Exam/SecondProblem-Crossword/solution.js
@@ -1,3 +1,33 @@
 function solve() {
-   // TODO
+   let inputElement = document.getElementById("input");
+ 
+   let buttons = document.getElementsByTagName("button");
+
+   let filterInput = buttons[0];
+   let sortInput = buttons[1];
+   let rotateInput = buttons[2];
+   let getInput = buttons[3];
+
+   filterInput.addEventListener("click", onClickFilter);
+   sortInput.addEventListener("click", onClickSort);
+   rotateInput.addEventListener("click", onClickRotate);
+   getInput.addEventListener("click", onClickGet);
+
+   let input = inputElement.textContent.split("");
+
+   function onClickFilter(event) {
+      let filterElements = event.target.parentNode.children;
+
+      let secondaryCommand = filterElements[1].children[1];
+      let selectedCommand = secondaryCommand[secondaryCommand.selectedIndex].value;
+      let position = filterElements[2].children[1].value;
+
+      switch(selectedCommand) {
+         case "uppercase": input.filter(letter => isUpperCase.letter);
+         default: break;
+         
+
+      }
+        
+   }
 }
