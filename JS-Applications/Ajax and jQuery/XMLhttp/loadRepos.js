2c69f9f4046f4c77a0b0f931c8f0c2203bfaf8e5
DimitarMilushev <DeadlyCookye@gmail.com>
2019-08-24 13:39:23 +0300

    Ajax and jQuery

---
+++ JS-Applications/Ajax and jQuery/XMLhttp/loadRepos.js
@@ -0,0 +1,21 @@
+function loadRepos() {
+
+let req = new XMLHttpRequest();
+
+req.onreadystatechange = function() {
+
+if (this.readyState == 4 && this.status == 200)
+
+document.getElementById("res").textContent =
+
+this.responseText;
+
+};
+
+req.open("GET",
+
+"https://api.github.com/users/DimitarMilushev/repos", true);
+
+req.send();
+
+}
